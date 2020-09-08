35c06c5ff566dd0e366ae979870ac944a55e0a38
Boxing <boxingp@gmail.com>
2020-09-08 09:55:16 +0800

    CT-1: create a script for tenant creation. Boxing

---
+++ create_tenant.py
@@ -0,0 +1,68 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+
+chrome_driver = webdriver.Chrome()
+tenant_name = "CLX169"
+
+
+def get_login_account(path):
+    file = open(path, "r")
+    lines = file.readlines()
+    username = lines[0].rstrip()
+    password = lines[1].rstrip()
+    file.close()
+    return {'username': username, 'password': password}
+
+
+def create_tenant(account, tenant, driver):
+    driver.get("http://corelimslite.thermofisher.cn/")
+
+    username_id = driver.find_element_by_id("lims_userNameID")
+    username_id.clear()
+    username_id.send_keys(account["username"])
+
+    password_id = driver.find_element_by_id("lims_passwordID")
+    password_id.clear()
+    password_id.send_keys(account["password"])
+
+    driver.find_element_by_id("lims_buttonID").click()
+
+    select_tenant = Select(driver.find_element_by_xpath("//select[@name='tenantSelect']"))
+    select_tenant.select_by_value("13")
+    driver.find_element_by_xpath("//input[@name='submit'][@type='submit']").click()
+
+    wait = WebDriverWait(driver, 420)
+    wait.until(EC.title_contains("PFS | Home"))
+
+    driver.get(
+        "http://corelimslite.thermofisher.cn/708646210/corelims?cmd=clone&entityType=PLATFORM%20ACCOUNT&entityId=17437965")
+
+    input_name = driver.find_element_by_id("name")
+    input_name.send_keys(tenant)
+
+    input_base_account_barcode = driver.find_element_by_id("17429591")
+    input_base_account_barcode.send_keys(Keys.CONTROL + "a")
+    input_base_account_barcode.send_keys(Keys.BACK_SPACE)
+    input_base_account_barcode.send_keys("ACT3@CI_LOOPBACK")
+
+    input_alias = driver.find_element_by_id("17429593")
+    input_alias.send_keys(Keys.CONTROL + "a")
+    input_alias.send_keys(Keys.BACK_SPACE)
+    input_alias.send_keys(tenant)
+
+    select_tomcat = Select(driver.find_element_by_id("associatedEntityIdentifier|17429833"))
+    select_tomcat.select_by_value("17430067")
+    driver.find_element_by_xpath("//input[@type='button'][@value='Save']").click()
+
+    wait.until(EC.title_contains("PFS | PLATFORM ACCOUNT Details"))
+
+    driver.get("http://corelimslite.thermofisher.cn/login?cmd=logout&entityType=LIMS")
+
+
+login_account = get_login_account("./account")
+create_tenant(login_account, tenant_name, chrome_driver)
+
+chrome_driver.close()
